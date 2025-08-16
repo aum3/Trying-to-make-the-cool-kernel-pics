3a2c08fc1e3565f2c4be3e1cd2f9b170427ebcd3
aum3 <aumd39@gmail.com>
2025-08-16 15:06:40 +0100

    first commit

---
+++ Creating MSH file.py
@@ -0,0 +1,10 @@
+#conda install wildmeshing
+#conda env's name is wildmesh_env and it specifically needs to be python=3.9 to 3.6 inclusive
+
+
+# import wildmeshing as wm
+# 
+# wm.tetrahedralize("Frog.stl", "Frog Buddha.msh")
+
+
+
